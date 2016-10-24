7b32642adc697dc3dea866943f574c72c32463dc
Mohsin Farooqui <mohsin.farooqui4@gmail.com>
2016-10-24 13:00:16 +0530

    committed

--- MFdivisibleby100/main.m
+++ MFdivisibleby100/main.m
@@ -6,12 +6,37 @@
 //  Copyright © 2016 Mohsin Farooqui. All rights reserved.
 //
 
+void divisible(int);
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        
+        
+            int number;
+            printf("Enter the number\n");
+            scanf("%d",&number);
+            divisible(number);
+        
+        
+        
+        
     }
     return 0;
+}
+
+void divisible(int number)
+{
+    if(number%100==0)
+    {
+        printf("The number is divisible by 100");
+    }
+    
+    else
+    {
+        printf("The number is not divisible by 100");
+    }
 }
